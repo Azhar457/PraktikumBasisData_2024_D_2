e2cc9dafaceb6237abb242f9337ed827507b86aa
Tegarstyw <tegardh123@gmail.com>
2024-12-22 14:57:29 +0700

    insert branch

---
+++ Data Collector/data.sql
@@ -0,0 +1,49 @@
+INSERT INTO branches (branch_name, address, city, province, postal_code, phone, opening_date) 
+VALUES 
+('BEBEK KALEYO CEMPAKA PUTIH', 'Jl. Cemp. Putih Tim. No.108, RW.3, Cemp. Putih Tim., Kec. Cemp. Putih', 'Kota Jakarta Pusat', 'DKI Jakarta', '10510', NULL, GETDATE()),
+
+('BEBEK KALEYO DAAN MOGOT', 'Jalan Kintamani Raya No.2, Kalideres', 'Kota Jakarta Barat', 'DKI Jakarta', '11840', NULL, GETDATE()),
+
+('BEBEK KALEYO SUNTER', 'Jl. Danau Sunter Utara No.27-28, RT.11/RW.12, Sunter Agung, Tj. Priok', 'Kota Jakarta Utara', 'DKI Jakarta', '14340', NULL, GETDATE()),
+
+('BEBEK KALEYO RAWAMANGUN', 'Jl. Pemuda No.290, RT.3/RW.1, Jati, Kec. Pulo Gadung', 'Kota Jakarta Timur', 'DKI Jakarta', '13220', NULL, GETDATE()),
+
+('BEBEK KALEYO BSD', 'Jl. Raya Rawa Buntu No.17, Rawa Buntu, Serpong', 'Kota Tangerang Selatan', 'Banten', '15318', NULL, GETDATE()),
+
+('BEBEK KALEYO TEBET', 'Jl. Lapangan Roos No.49, RT.4/RW.1, Tebet Tim.', 'Kota Jakarta Selatan', 'DKI Jakarta', '12840', NULL, GETDATE()),
+
+('BEBEK KALEYO HARAPAN INDAH', 'Jl. Harapan Indah Boulevard No.9e, Medan Satria', 'Kota Bekasi', 'Jawa Barat', '17132', NULL, GETDATE()),
+
+('BEBEK KALEYO JATIWARINGIN', 'Jl. Raya Jatiwaringin No.24, Jatiwaringin, Kec. Pondokgede', 'Kota Bekasi', 'Jawa Barat', '17411', NULL, GETDATE()),
+
+('BEBEK KALEYO BINTARO', 'Jl. Pondok Jaya no.10 Bintaro Sektor 9 Seberang Pasar Modern Bintaro', 'Kota Tangerang Selatan', 'Banten', '15229', NULL, GETDATE()),
+
+('BEBEK KALEYO KALIMALANG', 'Ruko Duta Permai Jalan Kyai Haji Noer Ali Sukadanau Cikarang Barat', 'Kota Bekasi', 'Jawa Barat', '17148', NULL, GETDATE()),
+
+('BEBEK KALEYO KEMBANGAN', 'Jl. Pesanggrahan Raya No.2b, RT.2/RW.7, Kembangan Sel.', 'Kota Jakarta Barat', 'DKI Jakarta', '11610', NULL, GETDATE()),
+
+('BEBEK KALEYO BANDUNG', 'Heaven, Komplek Bali, Jl. Pasir Kaliki No.185 - 189, Pamoyanan', 'Kota Bandung', 'Jawa Barat', '40173', NULL, GETDATE()),
+
+('BEBEK KALEYO GRANDWISATA', 'Kawasan Kuliner Cafewalk , Jalan Grand Wisata Tambun', 'Kota Bekasi', 'Jawa Barat', '17510', NULL, GETDATE()),
+
+('BEBEK KALEYO KARAWANG', 'Jl. Akses Tol Karawang Bar., Purwadana', 'Kabupaten Karawang', 'Jawa Barat', '41361', NULL, GETDATE()),
+
+('BEBEK KALEYO GADING SERPONG', 'Ruko B1 Jln. Boulevard Raya BA4 No. 27-28 Gading Serpong', 'Kota Tangerang Selatan', 'Banten', NULL, NULL, GETDATE()),
+
+('BEBEK KALEYO CIKARANG', 'Jl. Majapahit No.18, Cibatu, Cikarang Sel.', 'Kota Bekasi', 'Jawa Barat', '17530', NULL, GETDATE()),
+
+('BEBEK KALEYO JABABEKA', 'Jalan Tarum Barat Raya Blok R No.1-2, Jayamukti', 'Kota Bekasi', 'Jawa Barat', '17530', NULL, GETDATE()),
+
+('BEBEK KALEYO LENTENG AGUNG', 'Jl. Lenteng Agung Raya No.5, RT.5/RW.1, Srengseng Sawah', 'Kota Jakarta Selatan', 'DKI Jakarta', '10550', NULL, GETDATE()),
+
+('BEBEK KALEYO KEMANGGISAN', 'Jl. Kemanggisan Raya. No. 39. Palmerah', 'Kota Jakarta Barat', 'DKI Jakarta', '11480', NULL, GETDATE()),
+
+('BEBEK KALEYO REST AREA PURWAKARTA', 'Jl. Rest Area KM 72, Cigelam', 'Kabupaten Purwakarta', 'Jawa Barat', '41151', NULL, GETDATE()),
+
+('BEBEK KALEYO CIBUBUR', 'Jl. Alternatif Cibubur No.100, Nagrak', 'Kabupaten Bogor', 'Jawa Barat', '16967', NULL, GETDATE()),
+
+('BEBEK KALEYO EXPRESS SUNTER', 'Ruko royal sunter blok D 16 Jln. Danau Sunter Selatan', 'Kota Jakarta Utara', 'DKI Jakarta', NULL, NULL, GETDATE()),
+
+('BEBEK KALEYO EXPRESS RASUNA', 'Rasuna Garden Food Street, Jl. Epicentrum Tengah No.3', 'Kota Jakarta Selatan', 'DKI Jakarta', '12940', NULL, GETDATE()),
+
+('BEBEK KALEYO KELAPA GADING', 'Jl. Boulevard Raya No.15-16 Blok TN', 'Kota Jakarta Utara', 'DKI Jakarta', '14240', NULL, GETDATE());
