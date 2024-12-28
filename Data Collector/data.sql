e9607d8d17087b3fef588bda433f7a12e4fdf52c
Tegarstyw <tegardh123@gmail.com>
2024-12-28 21:31:40 +0700

    upload data

--- Data Collector/data.sql
+++ Data Collector/data.sql
@@ -1,51 +1,164 @@
-INSERT INTO branches (branch_name, address, city, province, postal_code, phone, opening_date) 
+INSERT INTO branches (branch_name, address, city, province, postal_code, opening_date) 
 VALUES 
-('BEBEK KALEYO CEMPAKA PUTIH', 'Jl. Cemp. Putih Tim. No.108, RW.3, Cemp. Putih Tim., Kec. Cemp. Putih', 'Kota Jakarta Pusat', 'DKI Jakarta', '10510', NULL, GETDATE()),
+('BEBEK KALEYO CEMPAKA PUTIH', 'Jl. Cemp. Putih Tim. No.108, RW.3, Cemp. Putih Tim., Kec. Cemp. Putih', 'Kota Jakarta Pusat', 'DKI Jakarta', '10510', GETDATE()),
+('BEBEK KALEYO DAAN MOGOT', 'Jalan Kintamani Raya No.2, Kalideres', 'Kota Jakarta Barat', 'DKI Jakarta', '11840', GETDATE()),
+('BEBEK KALEYO SUNTER', 'Jl. Danau Sunter Utara No.27-28, RT.11/RW.12, Sunter Agung, Tj. Priok', 'Kota Jakarta Utara', 'DKI Jakarta', '14340', GETDATE()),
+('BEBEK KALEYO RAWAMANGUN', 'Jl. Pemuda No.290, RT.3/RW.1, Jati, Kec. Pulo Gadung', 'Kota Jakarta Timur', 'DKI Jakarta', '13220', GETDATE()),
+('BEBEK KALEYO BSD', 'Jl. Raya Rawa Buntu No.17, Rawa Buntu, Serpong', 'Kota Tangerang Selatan', 'Banten', '15318', GETDATE()),
+('BEBEK KALEYO TEBET', 'Jl. Lapangan Roos No.49, RT.4/RW.1, Tebet Tim.', 'Kota Jakarta Selatan', 'DKI Jakarta', '12840', GETDATE()),
+('BEBEK KALEYO HARAPAN INDAH', 'Jl. Harapan Indah Boulevard No.9e, Medan Satria', 'Kota Bekasi', 'Jawa Barat', '17132', GETDATE()),
+('BEBEK KALEYO JATIWARINGIN', 'Jl. Raya Jatiwaringin No.24, Jatiwaringin, Kec. Pondokgede', 'Kota Bekasi', 'Jawa Barat', '17411', GETDATE()),
+('BEBEK KALEYO BINTARO', 'Jl. Pondok Jaya no.10 Bintaro Sektor 9 Seberang Pasar Modern Bintaro', 'Kota Tangerang Selatan', 'Banten', '15229', GETDATE()),
+('BEBEK KALEYO KALIMALANG', 'Ruko Duta Permai Jalan Kyai Haji Noer Ali Sukadanau Cikarang Barat', 'Kota Bekasi', 'Jawa Barat', '17148', GETDATE()),
+('BEBEK KALEYO KEMBANGAN', 'Jl. Pesanggrahan Raya No.2b, RT.2/RW.7, Kembangan Sel.', 'Kota Jakarta Barat', 'DKI Jakarta', '11610', GETDATE()),
+('BEBEK KALEYO BANDUNG', 'Heaven, Komplek Bali, Jl. Pasir Kaliki No.185 - 189, Pamoyanan', 'Kota Bandung', 'Jawa Barat', '40173', GETDATE()),
+('BEBEK KALEYO GRANDWISATA', 'Kawasan Kuliner Cafewalk , Jalan Grand Wisata Tambun', 'Kota Bekasi', 'Jawa Barat', '17510', GETDATE()),
+('BEBEK KALEYO KARAWANG', 'Jl. Akses Tol Karawang Bar., Purwadana', 'Kabupaten Karawang', 'Jawa Barat', '41361', GETDATE()),
+('BEBEK KALEYO CIKARANG', 'Jl. Majapahit No.18, Cibatu, Cikarang Sel.', 'Kota Bekasi', 'Jawa Barat', '17530', GETDATE()),
+('BEBEK KALEYO JABABEKA', 'Jalan Tarum Barat Raya Blok R No.1-2, Jayamukti', 'Kota Bekasi', 'Jawa Barat', '17530', GETDATE()),
+('BEBEK KALEYO LENTENG AGUNG', 'Jl. Lenteng Agung Raya No.5, RT.5/RW.1, Srengseng Sawah', 'Kota Jakarta Selatan', 'DKI Jakarta', '10550', GETDATE()),
+('BEBEK KALEYO KEMANGGISAN', 'Jl. Kemanggisan Raya. No. 39. Palmerah', 'Kota Jakarta Barat', 'DKI Jakarta', '11480', GETDATE()),
+('BEBEK KALEYO REST AREA PURWAKARTA', 'Jl. Rest Area KM 72, Cigelam', 'Kabupaten Purwakarta', 'Jawa Barat', '41151', GETDATE()),
+('BEBEK KALEYO CIBUBUR', 'Jl. Alternatif Cibubur No.100, Nagrak', 'Kabupaten Bogor', 'Jawa Barat', '16967', GETDATE()),
+('BEBEK KALEYO EXPRESS RASUNA', 'Rasuna Garden Food Street, Jl. Epicentrum Tengah No.3', 'Kota Jakarta Selatan', 'DKI Jakarta', '12940', GETDATE()),
+('BEBEK KALEYO KELAPA GADING', 'Jl. Boulevard Raya No.15-16 Blok TN', 'Kota Jakarta Utara', 'DKI Jakarta', '14240', GETDATE()),
+('BEBEK KALEYO GARDENIA', 'Jl. Gardenia Raya No.3, RT.7/RW.8, Jakarta Timur', 'Kota Jakarta Timur', 'DKI Jakarta', '13350', GETDATE()),
+('BEBEK KALEYO CIKARANG BARAT', 'Jl. Raya Cikarang Barat No.12, Cikarang', 'Kabupaten Bekasi', 'Jawa Barat', '17310', GETDATE()),
+('BEBEK KALEYO TANGERANG', 'Jl. Tangerang Selatan No.7, Pondok Aren', 'Kota Tangerang Selatan', 'Banten', '15310', GETDATE()),
+('BEBEK KALEYO RUKO SUMBER', 'Jl. Sumber Raya No.22, Sumber', 'Kota Serang', 'Banten', '42112', GETDATE()),
+('BEBEK KALEYO KELAPA GADING 2', 'Jl. Kelapa Gading No.10, RT.3/RW.6, Jakarta Utara', 'Kota Jakarta Utara', 'DKI Jakarta', '14230', GETDATE()),
+('BEBEK KALEYO EXPRESS SUNTER', 'Ruko royal sunter blok D 16 Jln. Danau Sunter Selatan', 'Kota Jakarta Utara', 'DKI Jakarta', NULL, GETDATE()),
+('BEBEK KALEYO EXPRESS RASUNA', 'Rasuna Garden Food Street, Jl. Epicentrum Tengah No.3', 'Kota Jakarta Selatan', 'DKI Jakarta', '12940',GETDATE()),
+('BEBEK KALEYO VILLA MAKMUR', 'Jl. Villa Makmur No.19, Sawangan', 'Kota Depok', 'Jawa Barat', '16417', GETDATE());
 
-('BEBEK KALEYO DAAN MOGOT', 'Jalan Kintamani Raya No.2, Kalideres', 'Kota Jakarta Barat', 'DKI Jakarta', '11840', NULL, GETDATE()),
+INSERT INTO daily_revenue (branch_id, revenue_date, total_revenue, total_customers, payment_method, notes)
+VALUES
+(1, '2024-12-01', 5000.00, 50, 'Cash', 'Good day'),
+(2, '2024-12-01', 3000.00, 30, 'Card', 'Average sales'),
+(3, '2024-12-02', 7000.00, 70, 'DANA', 'Promotions applied'),
+(4, '2024-12-02', 6500.00, 65, 'Cash', 'Strong sales'),
+(5, '2024-12-03', 4000.00, 40, 'Card', 'Steady performance'),
+(6, '2024-12-03', 5500.00, 55, 'DANA', 'Holiday boost'),
+(7, '2024-12-04', 3200.00, 32, 'Cash', 'Rainy day'),
+(8, '2024-12-04', 4800.00, 48, 'Card', 'Good customer turnout'),
+(9, '2024-12-05', 3000.00, 30, 'DANA', 'Promotional discount'),
+(10, '2024-12-05', 3700.00, 37, 'Cash', 'Weekend sales'),
+(11, '2024-12-06', 4200.00, 42, 'Card', 'Holiday season'),
+(12, '2024-12-06', 5000.00, 50, 'DANA', 'Great weather'),
+(13, '2024-12-07', 6000.00, 60, 'Cash', 'High traffic'),
+(14, '2024-12-07', 3100.00, 31, 'Card', 'Steady day'),
+(15, '2024-12-08', 4600.00, 46, 'DANA', 'Marketing success'),
+(16, '2024-12-08', 3900.00, 39, 'Cash', 'Good flow'),
+(17, '2024-12-09', 5300.00, 53, 'Card', 'Regular customers'),
+(18, '2024-12-09', 4700.00, 47, 'DANA', 'Loyalty program boost'),
+(19, '2024-12-10', 3400.00, 34, 'Cash', 'Slow morning'),
+(20, '2024-12-10', 5600.00, 56, 'Card', 'Evening surge'),
+(21, '2024-12-11', 4800.00, 48, 'DANA', 'Holiday prep'),
+(22, '2024-12-11', 6200.00, 62, 'Cash', 'Large orders'),
+(23, '2024-12-12', 5800.00, 58, 'Card', 'Steady pace'),
+(24, '2024-12-12', 4900.00, 49, 'DANA', 'Good weather'),
+(25, '2024-12-13', 4500.00, 45, 'Cash', 'Regular flow'),
+(26, '2024-12-13', 5200.00, 52, 'Card', 'Weekend boost'),
+(27, '2024-12-14', 6100.00, 61, 'DANA', 'Promotions ongoing'),
+(28, '2024-12-14', 4100.00, 41, 'Cash', 'Steady morning'),
+(29, '2024-12-15', 4300.00, 43, 'Card', 'Afternoon rush'),
+(30, '2024-12-15', 5700.00, 57, 'DANA', 'Pre-holiday shopping');
 
-('BEBEK KALEYO SUNTER', 'Jl. Danau Sunter Utara No.27-28, RT.11/RW.12, Sunter Agung, Tj. Priok', 'Kota Jakarta Utara', 'DKI Jakarta', '14340', NULL, GETDATE()),
+INSERT INTO menu (menu_name, category, price) 
+VALUES 
+('Bebek Goreng', 'Makanan', 25000.00),
+('Bebek Bakar', 'Makanan', 30000.00),
+('Ayam Goreng', 'Makanan', 22000.00),
+('Ayam Bakar', 'Makanan', 27000.00),
+('Sate Ayam', 'Makanan', 20000.00),
+('Sate Bebek', 'Makanan', 35000.00),
+('Tahu Tempe', 'Makanan', 10000.00),
+('Nasi Goreng', 'Makanan', 15000.00),
+('Nasi Campur', 'Makanan', 18000.00),
+('Nasi Uduk', 'Makanan', 12000.00),
+('Gulai Bebek', 'Makanan', 32000.00),
+('Gulai Ayam', 'Makanan', 28000.00),
+('Sup Bebek', 'Makanan', 27000.00),
+('Sup Ayam', 'Makanan', 23000.00),
+('Kerupuk', 'Cemilan', 5000.00),
+('Pisang Goreng', 'Cemilan', 8000.00),
+('Pisang Bakar', 'Cemilan', 10000.00),
+('Roti Bakar', 'Cemilan', 7000.00),
+('Martabak Telur', 'Cemilan', 15000.00),
+('Martabak Manis', 'Cemilan', 18000.00),
+('Es Teh Manis', 'Minuman', 7000.00),
+('Es Jeruk', 'Minuman', 8000.00),
+('Es Kelapa Muda', 'Minuman', 12000.00),
+('Es Milo', 'Minuman', 10000.00),
+('Kopi Hitam', 'Minuman', 15000.00),
+('Kopi Susu', 'Minuman', 17000.00),
+('Cappuccino', 'Minuman', 18000.00),
+('Latte', 'Minuman', 19000.00),
+('Teh Tarik', 'Minuman', 9000.00),
+('Air Mineral', 'Minuman', 5000.00);
 
-('BEBEK KALEYO RAWAMANGUN', 'Jl. Pemuda No.290, RT.3/RW.1, Jati, Kec. Pulo Gadung', 'Kota Jakarta Timur', 'DKI Jakarta', '13220', NULL, GETDATE()),
+INSERT INTO sales (branch_id, menu_id, sale_date, quantity, total_price, payment_method)
+VALUES
+(1, 5, '2024-12-01 10:30:00', 3, 75000.00, 'Cash'),
+(2, 10, '2024-12-01 11:45:00', 4, 160000.00, 'DANA'),
+(3, 15, '2024-12-01 12:20:00', 2, 90000.00, 'OVO'),
+(4, 20, '2024-12-02 10:10:00', 5, 225000.00, 'Cash'),
+(5, 25, '2024-12-02 13:15:00', 3, 120000.00, 'DANA'),
+(6, 3, '2024-12-02 15:00:00', 2, 60000.00, 'OVO'),
+(7, 8, '2024-12-03 09:40:00', 4, 160000.00, 'Cash'),
+(8, 13, '2024-12-03 11:30:00', 6, 270000.00, 'DANA'),
+(9, 18, '2024-12-03 14:00:00', 1, 45000.00, 'OVO'),
+(10, 23, '2024-12-04 12:10:00', 3, 135000.00, 'Cash'),
+(11, 28, '2024-12-04 13:25:00', 5, 250000.00, 'DANA'),
+(12, 30, '2024-12-04 14:40:00', 2, 80000.00, 'OVO'),
+(13, 4, '2024-12-05 10:30:00', 3, 90000.00, 'Cash'),
+(14, 9, '2024-12-05 12:20:00', 2, 70000.00, 'DANA'),
+(15, 14, '2024-12-05 13:45:00', 4, 200000.00, 'OVO'),
+(16, 19, '2024-12-06 11:10:00', 3, 135000.00, 'Cash'),
+(17, 24, '2024-12-06 14:15:00', 5, 225000.00, 'DANA'),
+(18, 6, '2024-12-06 15:30:00', 2, 60000.00, 'OVO'),
+(19, 11, '2024-12-07 09:50:00', 4, 160000.00, 'Cash'),
+(20, 16, '2024-12-07 12:30:00', 6, 300000.00, 'DANA'),
+(21, 21, '2024-12-07 13:45:00', 3, 120000.00, 'OVO'),
+(22, 26, '2024-12-08 11:15:00', 1, 50000.00, 'Cash'),
+(23, 2, '2024-12-08 13:00:00', 4, 180000.00, 'DANA'),
+(24, 7, '2024-12-08 14:40:00', 3, 135000.00, 'OVO'),
+(25, 12, '2024-12-09 10:00:00', 5, 250000.00, 'Cash'),
+(26, 17, '2024-12-09 11:30:00', 2, 90000.00, 'DANA'),
+(27, 22, '2024-12-09 13:20:00', 3, 120000.00, 'OVO'),
+(28, 27, '2024-12-10 12:45:00', 4, 160000.00, 'Cash'),
+(29, 1, '2024-12-10 14:00:00', 6, 300000.00, 'DANA'),
+(30, 10, '2024-12-10 15:15:00', 3, 135000.00, 'OVO');
 
-('BEBEK KALEYO BSD', 'Jl. Raya Rawa Buntu No.17, Rawa Buntu, Serpong', 'Kota Tangerang Selatan', 'Banten', '15318', NULL, GETDATE()),
-
-('BEBEK KALEYO TEBET', 'Jl. Lapangan Roos No.49, RT.4/RW.1, Tebet Tim.', 'Kota Jakarta Selatan', 'DKI Jakarta', '12840', NULL, GETDATE()),
-
-('BEBEK KALEYO HARAPAN INDAH', 'Jl. Harapan Indah Boulevard No.9e, Medan Satria', 'Kota Bekasi', 'Jawa Barat', '17132', NULL, GETDATE()),
-
-('BEBEK KALEYO JATIWARINGIN', 'Jl. Raya Jatiwaringin No.24, Jatiwaringin, Kec. Pondokgede', 'Kota Bekasi', 'Jawa Barat', '17411', NULL, GETDATE()),
-
-('BEBEK KALEYO BINTARO', 'Jl. Pondok Jaya no.10 Bintaro Sektor 9 Seberang Pasar Modern Bintaro', 'Kota Tangerang Selatan', 'Banten', '15229', NULL, GETDATE()),
-
-('BEBEK KALEYO KALIMALANG', 'Ruko Duta Permai Jalan Kyai Haji Noer Ali Sukadanau Cikarang Barat', 'Kota Bekasi', 'Jawa Barat', '17148', NULL, GETDATE()),
-
-('BEBEK KALEYO KEMBANGAN', 'Jl. Pesanggrahan Raya No.2b, RT.2/RW.7, Kembangan Sel.', 'Kota Jakarta Barat', 'DKI Jakarta', '11610', NULL, GETDATE()),
-
-('BEBEK KALEYO BANDUNG', 'Heaven, Komplek Bali, Jl. Pasir Kaliki No.185 - 189, Pamoyanan', 'Kota Bandung', 'Jawa Barat', '40173', NULL, GETDATE()),
-
-('BEBEK KALEYO GRANDWISATA', 'Kawasan Kuliner Cafewalk , Jalan Grand Wisata Tambun', 'Kota Bekasi', 'Jawa Barat', '17510', NULL, GETDATE()),
-
-('BEBEK KALEYO KARAWANG', 'Jl. Akses Tol Karawang Bar., Purwadana', 'Kabupaten Karawang', 'Jawa Barat', '41361', NULL, GETDATE()),
-
-('BEBEK KALEYO GADING SERPONG', 'Ruko B1 Jln. Boulevard Raya BA4 No. 27-28 Gading Serpong', 'Kota Tangerang Selatan', 'Banten', NULL, NULL, GETDATE()),
-
-('BEBEK KALEYO CIKARANG', 'Jl. Majapahit No.18, Cibatu, Cikarang Sel.', 'Kota Bekasi', 'Jawa Barat', '17530', NULL, GETDATE()),
-
-('BEBEK KALEYO JABABEKA', 'Jalan Tarum Barat Raya Blok R No.1-2, Jayamukti', 'Kota Bekasi', 'Jawa Barat', '17530', NULL, GETDATE()),
-
-('BEBEK KALEYO LENTENG AGUNG', 'Jl. Lenteng Agung Raya No.5, RT.5/RW.1, Srengseng Sawah', 'Kota Jakarta Selatan', 'DKI Jakarta', '10550', NULL, GETDATE()),
-
-('BEBEK KALEYO KEMANGGISAN', 'Jl. Kemanggisan Raya. No. 39. Palmerah', 'Kota Jakarta Barat', 'DKI Jakarta', '11480', NULL, GETDATE()),
-
-('BEBEK KALEYO REST AREA PURWAKARTA', 'Jl. Rest Area KM 72, Cigelam', 'Kabupaten Purwakarta', 'Jawa Barat', '41151', NULL, GETDATE()),
-
-('BEBEK KALEYO CIBUBUR', 'Jl. Alternatif Cibubur No.100, Nagrak', 'Kabupaten Bogor', 'Jawa Barat', '16967', NULL, GETDATE()),
-
-('BEBEK KALEYO EXPRESS SUNTER', 'Ruko royal sunter blok D 16 Jln. Danau Sunter Selatan', 'Kota Jakarta Utara', 'DKI Jakarta', NULL, NULL, GETDATE()),
-
-('BEBEK KALEYO EXPRESS RASUNA', 'Rasuna Garden Food Street, Jl. Epicentrum Tengah No.3', 'Kota Jakarta Selatan', 'DKI Jakarta', '12940', NULL, GETDATE()),
-
-('BEBEK KALEYO KELAPA GADING', 'Jl. Boulevard Raya No.15-16 Blok TN', 'Kota Jakarta Utara', 'DKI Jakarta', '14240', NULL, GETDATE());
-
--- pppppppppppppp
+INSERT INTO operational_costs (branch_id, cost_date, cost_category, amount, notes)
+VALUES
+(1, '2024-12-01', 'Electricity', 1500000.00, 'Monthly electricity bill'),
+(2, '2024-12-01', 'Water', 500000.00, 'Monthly water bill'),
+(3, '2024-12-02', 'Rent', 10000000.00, 'Monthly rental payment'),
+(4, '2024-12-02', 'Supplies', 2000000.00, 'Purchase of raw materials'),
+(5, '2024-12-03', 'Salaries', 15000000.00, 'Staff salaries for December'),
+(6, '2024-12-03', 'Marketing', 3000000.00, 'Promotional campaign'),
+(7, '2024-12-04', 'Repairs', 1200000.00, 'Kitchen equipment repair'),
+(8, '2024-12-04', 'Transport', 800000.00, 'Delivery expenses'),
+(9, '2024-12-05', 'Electricity', 1700000.00, 'Monthly electricity bill'),
+(10, '2024-12-05', 'Water', 600000.00, 'Monthly water bill'),
+(11, '2024-12-06', 'Rent', 9500000.00, 'Monthly rental payment'),
+(12, '2024-12-06', 'Supplies', 2200000.00, 'Raw materials restock'),
+(13, '2024-12-07', 'Salaries', 14000000.00, 'December staff payments'),
+(14, '2024-12-07', 'Marketing', 3500000.00, 'Holiday promotion'),
+(15, '2024-12-08', 'Repairs', 1000000.00, 'Air conditioner maintenance'),
+(16, '2024-12-08', 'Transport', 900000.00, 'Fuel and vehicle maintenance'),
+(17, '2024-12-09', 'Electricity', 1600000.00, 'Electricity bill adjustment'),
+(18, '2024-12-09', 'Water', 550000.00, 'Water usage surcharge'),
+(19, '2024-12-10', 'Rent', 10500000.00, 'Rental agreement renewal'),
+(20, '2024-12-10', 'Supplies', 2500000.00, 'Holiday stock preparation'),
+(21, '2024-12-11', 'Salaries', 15500000.00, 'Year-end bonuses included'),
+(22, '2024-12-11', 'Marketing', 4000000.00, 'Social media advertisements'),
+(23, '2024-12-12', 'Repairs', 800000.00, 'Minor building repairs'),
+(24, '2024-12-12', 'Transport', 750000.00, 'Logistics expenses'),
+(25, '2024-12-13', 'Electricity', 1800000.00, 'High usage month'),
+(26, '2024-12-13', 'Water', 650000.00, 'December water bill'),
+(27, '2024-12-14', 'Rent', 11000000.00, 'New branch lease'),
+(28, '2024-12-14', 'Supplies', 2300000.00, 'Extra stock for holiday sales'),
+(29, '2024-12-15', 'Salaries', 16000000.00, 'Employee overtime'),
+(30, '2024-12-15', 'Marketing', 4500000.00, 'Event sponsorships');
